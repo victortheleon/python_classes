9a88cf21e10eedf82d25dde69c689fab6d1e9661
Manish <victortheleon@hotmail.com>
2020-07-12 09:12:14 -0700

    July 12

--- ch11.py
+++ ch11.py
@@ -4,5 +4,20 @@ def example2():
     global been_called
     been_called = True
 
-example2()
-print(been_called)
+# example2()
+# print(been_called)
+
+def sort_words_by_length():
+    fin = open('words.txt')
+    lt = []
+    for line in fin:
+        line = line.strip()
+        lt.append((len(line), line))
+    lt.sort(reverse=True)
+    res = []
+    for _, word in lt:
+        res.append(word)
+    return res
+
+x = sort_words_by_length()
+print(x[0])
